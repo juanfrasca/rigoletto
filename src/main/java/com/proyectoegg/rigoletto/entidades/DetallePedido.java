9bb74c008f877d34271101037f4310c0baaac8fb
juanfrasca <juanfrasca@gmail.com>
2019-12-05 20:26:54 -0300

    Update DetallePedido.java
    Getter & Setter de DetallePedido

--- src/main/java/com/proyectoegg/rigoletto/entidades/DetallePedido.java
+++ src/main/java/com/proyectoegg/rigoletto/entidades/DetallePedido.java
@@ -6,6 +6,29 @@ public class DetallePedido {
 
     private Integer cantidad;
 
-    public void untitledMethod() {
+    public Integer getIdDetallePedido() {
+        return idDetallePedido;
     }
+
+    public void setIdDetallePedido(Integer idDetallePedido) {
+        this.idDetallePedido = idDetallePedido;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    
 }
