fdaf69db87135338164f4bdc23be40868d9f0715
everlagos <58532855+everlagos@users.noreply.github.com>
2019-12-06 16:58:03 -0300

    pedido
    los geters y seters

--- src/main/java/com/proyectoegg/rigoletto/entidades/Pedido.java
+++ src/main/java/com/proyectoegg/rigoletto/entidades/Pedido.java
@@ -41,4 +41,76 @@ public class Pedido {
     public double getmontoTotal() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    public Integer getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(Integer idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Date getFecha_hora() {
+        return fecha_hora;
+    }
+
+    public void setFecha_hora(Date fecha_hora) {
+        this.fecha_hora = fecha_hora;
+    }
+
+    public MedioDePago getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(MedioDePago medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public Long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Long telefono) {
+        this.telefono = telefono;
+    }
+
+    public TipoEntrega getTipoEntrega() {
+        return tipoEntrega;
+    }
+
+    public void setTipoEntrega(TipoEntrega tipoEntrega) {
+        this.tipoEntrega = tipoEntrega;
+    }
+
+    public List<DetallePedido> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<DetallePedido> productos) {
+        this.productos = productos;
+    }
 }
