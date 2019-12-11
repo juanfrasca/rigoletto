57a8d017ebe9ed510a9b7673a8f43f88627139cf
PabloForto <58532413+PabloForto@users.noreply.github.com>
2019-12-11 20:41:50 -0300

    Usuario controlador
    se realizo el usuario controlador para pode realizar el login

--- src/main/java/com/proyectoegg/rigoletto/Controladores/LandingControler.java
+++ src/main/java/com/proyectoegg/rigoletto/Controladores/LandingControler.java
@@ -5,7 +5,6 @@ import com.proyectoegg.rigoletto.servicios.UsuarioServicios;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,13 +20,6 @@ public class LandingControler {
         return "index.html";
     }
 
-    
-    @PostMapping("/registrar")
-    public String Registro(@RequestParam String nombre, @RequestParam String apellido, @RequestParam String domicilio, @RequestParam String telefono, @RequestParam String email, @RequestParam String clave, @RequestParam String zona) throws ErrorServicio {
-        usuarioservicio.crearUsuario(nombre, apellido, domicilio, telefono, email, clave, zona);
-        return "redirect:/";
-    }
-    
      @GetMapping("/eliminar")
     public String eliminar(@RequestParam String id) {
         try {
