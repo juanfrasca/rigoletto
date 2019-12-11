3ecbac84dd433f7765f7fc3145d312181787d486
PabloForto <58532413+PabloForto@users.noreply.github.com>
2019-12-11 18:31:47 -0300

    Update PedidoRepositorio.java

--- src/main/java/com/proyectoegg/rigoletto/repositorios/PedidoRepositorio.java
+++ src/main/java/com/proyectoegg/rigoletto/repositorios/PedidoRepositorio.java
@@ -14,3 +14,4 @@ public interface PedidoRepositorio extends JpaRepository<Pedido, Integer>{
     @Query("SELECT c FROM Pedido c WHERE c.usuario=:q")
     public List<Pedido> buscarPedido(@Param("q") Usuario q);
 }
+adsfhyiuaydfhsdifjisldfjsdlfjsdlfkj
