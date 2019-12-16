0edbfcc2d2874c6a133761e394a7af408e1b99b5
everlagos <58532855+everlagos@users.noreply.github.com>
2019-12-16 17:01:50 -0300

    usuarios
    actualizacion y midificacion de los usuarios

--- src/main/java/com/proyectoegg/rigoletto/Controladores/UsuarioControlador.java
+++ src/main/java/com/proyectoegg/rigoletto/Controladores/UsuarioControlador.java
@@ -6,6 +6,7 @@ import com.proyectoegg.rigoletto.repositorios.UsuarioRepositorio;
 import com.proyectoegg.rigoletto.servicios.UsuarioServicios;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,5 +58,28 @@ public class UsuarioControlador {
         return "redirect:/registro";
     }
 
+    @GetMapping("/editarUsuario")
+    public String actualizacion(@RequestParam(required = false) String id, @RequestParam(required = false) String error,@RequestParam String nombre,@RequestParam String apellido,@RequestParam String domicilio,@RequestParam String telefono,@RequestParam String email,@RequestParam String clave,@RequestParam String zona,ModelMap modelo) {
+        if (id != null) {
+            Usuario usuario = us.buscarUsuarios(id);
+            modelo.put("asuario", usuario);
+        } else {
+            modelo.put("usuario", new Usuario());
+        }
 
+        modelo.put("error", error);
+
+        return "indexL.html";
+    }
+    @PostMapping("/registro")
+    public String actualizar(@RequestParam(required = false) String id, @RequestParam String nombre, @RequestParam String apellido,@RequestParam String domicilio,@RequestParam String telefono,@RequestParam String email,@RequestParam String clave,@RequestParam String zona,ModelMap modelo) {
+
+        try {
+            us.modificar(id, nombre, apellido, domicilio,telefono,email,clave,zona);
+        } catch (Exception ex) {
+            return "redirect:indexL" + id + "&error=" + ex.getMessage();
+        }
+
+        return "redirect:/index";
+    }
 }
