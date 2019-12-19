1f53f218987be74ba62ce265ab9ba0171d3e9d0a
PabloForto <58532413+PabloForto@users.noreply.github.com>
2019-12-19 19:59:34 -0300

    Update ProductoServicios.java
    modificacion

--- src/main/java/com/proyectoegg/rigoletto/servicios/ProductoServicios.java
+++ src/main/java/com/proyectoegg/rigoletto/servicios/ProductoServicios.java
@@ -10,55 +10,55 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ProductoServicios {
-
+    
     @Autowired
     ProductoRepositorio productorepositorio;
     
-
     @Transactional
-    public void crearProducto(String nombre, Double precio, String tipoProducto) throws ErrorServicio {
-
-        validar(nombre, precio,tipoProducto);
-
+    public void crearProducto(String nombre, Double precio, String tipoProducto, String foto) throws ErrorServicio {
+        
+        validar(nombre, precio, tipoProducto);
+        
         Producto producto = new Producto();
-
+        
         producto.setNombre(nombre);
         producto.setPrecio(precio);
         producto.setTipoProducto(tipoProducto);
+        producto.setFoto(foto);
         productorepositorio.save(producto);
     }
-
+    
     @Transactional
     public void modificarProducto(String idProducto, String nombre, Double precio, String tipoProducto) throws ErrorServicio {
-        validar(nombre, precio,tipoProducto);
+        validar(nombre, precio, tipoProducto);
         Optional<Producto> respuesta = productorepositorio.findById(idProducto);
         if (respuesta.isPresent()) {
             Producto producto = new Producto();
             producto.setNombre(nombre);
             producto.setPrecio(precio);
-
+            producto.setFoto(nombre);
+            producto.setTipoProducto(tipoProducto);
+            
             productorepositorio.save(producto);
-
+            
         } else {
             throw new ErrorServicio("No se a encontrado el producto solicitado.");
-
+            
         }
         
-        
-
     }
-
-    public void validar(String nombre, Double precio,String tipoProducto) throws ErrorServicio {
-
+    
+    public void validar(String nombre, Double precio, String tipoProducto) throws ErrorServicio {
+        
         if (nombre == null || nombre.isEmpty()) {
             throw new ErrorServicio("El nombre del producto no puede ser nulo.");
         }
-
+        
         if (precio == null) {
             throw new ErrorServicio("El precio no puede ser nulo.");
         }
         
-        if(tipoProducto == null || tipoProducto.isEmpty()){
+        if (tipoProducto == null || tipoProducto.isEmpty()) {
             throw new ErrorServicio("El tipo de producto no puede ser nulo.");
         }
     }
